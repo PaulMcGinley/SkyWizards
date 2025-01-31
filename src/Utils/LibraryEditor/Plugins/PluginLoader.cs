b6a2a88a19aed29e51ad6a918ad0aa58a27e2918
PaulMcGinley <41758581+PaulMcGinley@users.noreply.github.com>
2025-01-31 13:09:38 +0000

    added system watch calls to handle hot swapable plugins

--- src/Utils/LibraryEditor/Plugins/PluginLoader.cs
+++ src/Utils/LibraryEditor/Plugins/PluginLoader.cs
@@ -3,21 +3,103 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Collections.Concurrent;
 
 namespace LibraryEditor.Plugins
 {
-    public class PluginLoader
+    public class PluginLoader : IDisposable
     {
         private readonly string pluginPath;
-        private readonly List<IPlugin> loadedPlugins;
+        private readonly ConcurrentDictionary<string, IPlugin> loadedPlugins;
+        private readonly FileSystemWatcher watcher;
+        private bool isDisposed;
+
+        public event EventHandler PluginsChanged;
 
         public PluginLoader(string _pluginPath)
         {
             pluginPath = _pluginPath;
-            loadedPlugins = new List<IPlugin>();
+            loadedPlugins = new ConcurrentDictionary<string, IPlugin>();
+
+            // Set up file system watcher
+            watcher = new FileSystemWatcher(pluginPath, "*.dll")
+            {
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime,
+                EnableRaisingEvents = true
+            };
+
+            watcher.Created += OnPluginChanged;
+            watcher.Changed += OnPluginChanged;
+            watcher.Deleted += OnPluginDeleted;
+            watcher.Renamed += OnPluginRenamed;
         }
 
-        public IReadOnlyList<IPlugin> LoadedPlugins => loadedPlugins.AsReadOnly();
+        public IReadOnlyList<IPlugin> LoadedPlugins => loadedPlugins.Values.ToList().AsReadOnly();
+
+        private void OnPluginChanged(object sender, FileSystemEventArgs e)
+        {
+            // Add a small delay to ensure the file is fully written
+            System.Threading.Thread.Sleep(100);
+            ReloadPlugin(e.FullPath);
+            PluginsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnPluginDeleted(object sender, FileSystemEventArgs e)
+        {
+            UnloadPlugin(e.FullPath);
+            PluginsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnPluginRenamed(object sender, RenamedEventArgs e)
+        {
+            UnloadPlugin(e.OldFullPath);
+            ReloadPlugin(e.FullPath);
+            PluginsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void UnloadPlugin(string pluginPath)
+        {
+            if (loadedPlugins.TryRemove(pluginPath, out var plugin))
+            {
+                // Cleanup if plugin implements IDisposable
+                if (plugin is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+
+        public void ReloadPlugin(string pluginPath)
+        {
+            try
+            {
+                UnloadPlugin(pluginPath);
+
+                Assembly assembly = Assembly.LoadFrom(pluginPath);
+                var pluginTypes = assembly.GetTypes()
+                    .Where(t => typeof(IPlugin).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+                foreach (Type pluginType in pluginTypes)
+                {
+                    try
+                    {
+                        if (Activator.CreateInstance(pluginType) is IPlugin plugin)
+                        {
+                            plugin.Initialize();
+                            loadedPlugins.TryAdd(pluginPath, plugin);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error creating plugin instance from {pluginType.Name}: {ex.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading plugin from {pluginPath}: {ex.Message}");
+            }
+        }
 
         public void LoadPlugins()
         {
@@ -32,35 +114,24 @@ namespace LibraryEditor.Plugins
 
             foreach (string dllPath in dllFiles)
             {
-                try
+                ReloadPlugin(dllPath);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                watcher.Dispose();
+                foreach (var plugin in loadedPlugins.Values)
                 {
-                    Assembly assembly = Assembly.LoadFrom(dllPath);
-
-                    // Find types that implement IPlugin
-                    var pluginTypes = assembly.GetTypes()
-                        .Where(t => typeof(IPlugin).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
-
-                    foreach (Type pluginType in pluginTypes)
+                    if (plugin is IDisposable disposable)
                     {
-                        try
-                        {
-                            // Create instance of the plugin
-                            if (Activator.CreateInstance(pluginType) is IPlugin plugin)
-                            {
-                                plugin.Initialize();
-                                loadedPlugins.Add(plugin);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Error creating plugin instance from {pluginType.Name}: {ex.Message}");
-                        }
+                        disposable.Dispose();
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error loading plugin from {dllPath}: {ex.Message}");
-                }
+                loadedPlugins.Clear();
+                isDisposed = true;
             }
         }
     }
